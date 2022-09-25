21c8d830b17e16eb66a31d606687b88547de50c5
autoxuexiplaywright-dev <180894403+autoxuexiplaywright-dev@users.noreply.github.com>
2022-09-25 12:37:04 +0800

    Allow override if enable gui in commandline args

--- autoxuexiplaywright/__main__.py
+++ autoxuexiplaywright/__main__.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import argparse
 from autoxuexiplaywright.utils import config, misc
 
 
@@ -7,12 +8,18 @@ def main():
     # entrance
     os.chdir(os.path.split(os.path.realpath(__file__))[0])
     runtime_config = config.get_runtime_config()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--gui", "-g", help="If enable GUI mode.", default=False, type=bool)
+    args = parser.parse_args()
+    if args.gui:
+        runtime_config.update({"gui": True})
     if runtime_config.get("gui", True):
         from autoxuexiplaywright.gui import api
-        api.start(*sys.argv, **runtime_config)
+        api.start(sys.argv, **runtime_config)
     else:
         misc.init_logger(**runtime_config)
-        misc.start_backend(*sys.argv, **runtime_config)
+        misc.start_backend(**runtime_config)
 
 
 if __name__ == "__main__":
