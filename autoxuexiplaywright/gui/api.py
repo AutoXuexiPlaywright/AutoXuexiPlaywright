de79479746dd1eb6537e3f2ef3706ec68e9651cc
autoxuexiplaywright-dev <180894403+autoxuexiplaywright-dev@users.noreply.github.com>
2022-09-26 13:05:56 +0800

    Use QTranslator to translate Qt native UI objects

--- autoxuexiplaywright/gui/api.py
+++ autoxuexiplaywright/gui/api.py
@@ -1,6 +1,7 @@
 import platform
 from autoxuexiplaywright.gui import ui
 from qtpy.QtWidgets import QApplication
+from qtpy.QtCore import QTranslator, QLibraryInfo
 from autoxuexiplaywright.defines import core
 
 if platform.system() == "Windows":
@@ -10,8 +11,19 @@ if platform.system() == "Windows":
 __all__ = ["start"]
 
 
+def lang_to_locale(lang: str) -> str:
+    parts = lang.split("-")
+    extra = "_"+parts[1].upper() if len(parts) > 1 else ""
+    return parts[0]+extra
+
+
 def start(argv: list, **kwargs):
     app = QApplication(argv)
+    lang = lang_to_locale(kwargs.get("lang", "zh-cn"))
+    translator = QTranslator()
+    translator.load(
+        "qt_"+lang, QLibraryInfo.location(QLibraryInfo.TranslationsPath))
+    app.installTranslator(translator)
     main_window = ui.MainWindow(**kwargs)
     main_window.show()
     app.exec_()
