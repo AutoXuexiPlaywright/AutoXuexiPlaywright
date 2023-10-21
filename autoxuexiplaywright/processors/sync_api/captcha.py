f668cdce6327a3dfc16a2e9653cf30a38e64a906
autoxuexiplaywright-dev <180894403+autoxuexiplaywright-dev@users.noreply.github.com>
2023-10-21 20:37:34 +0800

    Remove captcha handling
    Due to that the old one is not working and we do
    not find a way to trigger it stablely to test
    new method.
    Will be readded when we can test the solution.

--- autoxuexiplaywright/processors/sync_api/captcha.py
+++ autoxuexiplaywright/processors/sync_api/captcha.py
@@ -4,19 +4,5 @@ from ..common.selectors import TestSelectors
 
 
 def handle_drag_captcha(captcha: Locator) -> bool:
-    target_x = 298
-    target_y = 32
-    slider = captcha.locator(TestSelectors.TEST_CAPTCHA_SLIDER)
-    target = captcha.locator(TestSelectors.TEST_CAPTCHA_TARGET)
-    target_box = target.bounding_box()
-    if target_box != None:
-        target_x = round(target_box["width"])
-        target_y = round(target_box["height"])
-    slider.drag_to(
-        target,
-        target_position={
-            "x": target_x,
-            "y": target_y
-        }
-    )
+    captcha.page.wait_for_timeout(300000)
     return captcha.is_hidden()
